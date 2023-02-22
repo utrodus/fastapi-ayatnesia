d26135483aea447766066a6376294b103f84e7ab
utrodus <come.programmer@gmail.com>
2023-02-22 22:14:02 +0700

    feat: add docs for json reader clas

--- src/json_reader.py
+++ src/json_reader.py
@@ -1,10 +1,19 @@
 import json
 
+
 class JSONReader:
     def __init__(self, file_path):
+        """Init method for JSONReader class. 
+        This constructor takes a file path as an argument and assigns it to the file_path attribute.
+        """
         self.file_path = file_path
 
     def read(self):
-        with open(self.file_path,  encoding="utf8") as f:
+        """Reads the JSON file and returns the data.
+        
+        Returns: 
+        Object: The data from the JSON file.
+        """        """"""
+        with open(self.file_path, encoding="utf8") as f:
             data = json.load(f)
         return data
