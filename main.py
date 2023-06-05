719b616b9f2115f24d91e67fee3bf419ef6cab09
utrodus <contact.utrodus@gmail.com>
2023-06-05 10:26:05 +0700

    feat: :zap: create endpoint for searching ayah with params and improve api docs

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import Field
+
+import sys
+sys.path.append("src")
 from src.database.database import test_connections, get_all_surahs, get_all_ayahs_by_surah_id
+from src.preprocessing.preprocessing import Preprocessing
+from src.similarity_measure.lexical.lexical_measure import LexicalMeasure
+
 app = FastAPI()
 app.title = (
-    "Quran Search API for searching Quranic verses with lexical and semantic features"
+    "Quran Search API for searching Quranic verses"
+)
+app.description = (
+    """
+    Quran Search API helps you to searching text using lexical, semantic, and lexical semantic measure. 🚀
+    
+### Search Feature
+You will be able to:
+
+* **Get All Qur'an Surahs** (✅ done).
+* **Get Detail Quran Surah** (✅ done).
+* **Search using lexical measures** (✅ done).
+* **Search using semantic measures** (⏳ _on progress_).
+* **Search using lexical semantic measures** (⏳ _on progress_).
+    """
 )
 app.version = "0.0.1"
 app.debug = True
@@ -28,3 +49,18 @@ async def get_surah(surah_id: int):
     ayahs = get_all_ayahs_by_surah_id(surah_id)
     return ayahs
 
+@app.post("/search")
+async def search(query: str, type: str = Query( "lexical", title="Search Type", description="type of search will be used: (**lexical** | **semantic** | **lexical_semantic**)"), limit: int = 5):
+    if(query == ""):
+        return {"message": "Kata kunci tidak boleh kosong."}
+    else:
+        query_preprocessed = Preprocessing(query).execute()
+        if(type == "lexical"):
+            lexical_measure = LexicalMeasure()
+            lexical_measure.calculate_lexical_similarity(query_preprocessed)
+            results = lexical_measure.get_top_similarities(limit)
+            return results
+        elif(type == "semantic"):
+            return {"query": query_preprocessed}
+        else:
+            return {"query": query_preprocessed}
