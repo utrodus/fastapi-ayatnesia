4a71965fd7f2a631c54c901908a5c65420f64de2
utrodus <contact.utrodus@gmail.com>
2023-06-12 00:17:51 +0700

    feat: :sparkles: experiment semantic similarity measure using fasttext

--- src/similarity_measure/semantic/experiments/fasttext_experiment.py
+++ src/similarity_measure/semantic/experiments/fasttext_experiment.py
@@ -1,47 +1,105 @@
+# import sys
+# import os
+# sys.path.append("src")
+# import compress_fasttext
+# import numpy as np
+
+# current_directory = os.path.dirname(__file__)
+
+# small_pretrained_model_path = os.path.abspath(os.path.join(current_directory, '../pretrained_model/cc.id.300.small.bin'))
+
+# model = compress_fasttext.models.CompressedFastTextKeyedVectors.load(small_pretrained_model_path)
+# # print(model.similarity('maksud', 'maksud'))
+
+# def calculate_semantic_similarity(query, documents):
+#     similarities = []
+#     for document in documents:
+#         document_words = document['preprocessed']
+#         document_embedding = np.mean([model[word] for word in document_words if word in model], axis=0)
+#         query_embedding = np.mean([model[word] for word in query if word in model], axis=0)
+#         similarity = np.dot(query_embedding, document_embedding) / (np.linalg.norm(query_embedding) * np.linalg.norm(document_embedding))
+#         similarities.append(similarity)
+
+#     # Calculate Jaccard similarity
+#     jaccard_similarities = [similarity / (2 - similarity) for similarity in similarities]
+
+#     # Sort documents based on similarity score
+#     sorted_documents = sorted(zip(documents, jaccard_similarities), key=lambda x: x[1], reverse=True)
+
+#     return sorted_documents
+
+# # Test the code with the given query and documents
+# query = ['allah', 'perkasa', 'seluruh', 'luas']
+# documents = [
+#     {"preprocessed": ["dengan", "nama", "allah", "maha", "asih", "maha", "sayang"]},
+#     {"preprocessed": ["puji", "allah", "tuhan", "alam"]},
+#     {"preprocessed": ["milik","hari","balas"]},
+#     {"preprocessed": ["maha", "besar", "allah", "tuhan", "semesta", "alam"]},
+#     {"preprocessed": ["tunjuk","kami","jalan","lurus"]},  
+# ]
+
+# results = calculate_semantic_similarity(query, documents)
+
+# # Display the similarity scores and sorted documents
+# for document, similarity in results:
+#     print(f"Similarity Score: {similarity}")
+#     print("Document:", document)
+#     print()
+
 import sys
 import os
 sys.path.append("src")
 import compress_fasttext
-
-from gensim.models.fasttext import load_facebook_vectors, load_facebook_model
 import numpy as np
 
-current_directory = os.path.dirname(__file__)
+class SemanticSimilarityCalculator:
+    def __init__(self, pretrained_model_path):
+        self.model = compress_fasttext.models.CompressedFastTextKeyedVectors.load(pretrained_model_path)
 
-pretrained_model_path = os.path.abspath(os.path.join(current_directory, '../pretrained_model/cc.id.300.bin'))
+    def calculate_cosine_similarity(self, vector1, vector2):
+        dot_product = np.dot(vector1, vector2)
+        norm_product = np.linalg.norm(vector1) * np.linalg.norm(vector2)
+        similarity = dot_product / norm_product
+        return similarity
+
+    def calculate_jaccard_similarity(self, similarity):
+        jaccard_similarity = similarity / (2 - similarity)
+        return jaccard_similarity
+
+    def calculate_similarity(self, query, documents):
+        similarities = []
+        for document in documents:
+            document_words = document['preprocessed']
+            document_embedding = np.mean([self.model[word] for word in document_words if word in self.model], axis=0)
+            query_embedding = np.mean([self.model[word] for word in query if word in self.model], axis=0)
+            similarity = self.calculate_cosine_similarity(query_embedding, document_embedding)
+            similarities.append(similarity)
+
+        jaccard_similarities = [self.calculate_jaccard_similarity(similarity) for similarity in similarities]
+
+        sorted_documents = sorted(zip(documents, jaccard_similarities), key=lambda x: x[1], reverse=True)
+
+        return sorted_documents
+
+
+# Test the code with the given query and documents
+current_directory = os.path.dirname(__file__)
 small_pretrained_model_path = os.path.abspath(os.path.join(current_directory, '../pretrained_model/cc.id.300.small.bin'))
 
-model = load_facebook_vectors(pretrained_model_path)
-# small_model = compress_fasttext.models.CompressedFastTextKeyedVectors.load(small_pretrained_model_path)
+query = ['allah', 'perkasa', 'seluruh', 'luas']
+documents = [
+    {"preprocessed": ["dengan", "nama", "allah", "maha", "asih", "maha", "sayang"]},
+    {"preprocessed": ["puji", "allah", "tuhan", "alam"]},
+    {"preprocessed": ["milik","hari","balas"]},
+    {"preprocessed": ["maha", "besar", "allah", "tuhan", "semesta", "alam"]},
+    {"preprocessed": ["tunjuk","kami","jalan","lurus"]},  
+]
 
-nearest_words = model.most_similar("tunjuk", topn=15)
-print(nearest_words)
+calculator = SemanticSimilarityCalculator(small_pretrained_model_path)
+results = calculator.calculate_similarity(query, documents)
 
-# # Fungsi untuk mendapatkan kata-kata terdekat dari dokumen/query
-# def get_similar_words(text):
-#     similar_words = []
-#     for word in text.split():
-#         try:
-#             similar = model.most_similar(word, topn=5)  # Ambil 5 kata terdekat
-#             similar_words.extend([word[0] for word in similar])
-#         except KeyError:
-#             continue
-#     return similar_words
-
-# # Fungsi untuk menghitung kemiripan semantik menggunakan Jaccard Measure
-# def semantic_similarity(query, document):
-#     query_words = set(get_similar_words(query))
-#     document_words = set(get_similar_words(document))
-    
-#     intersection = len(query_words.intersection(document_words))
-#     union = len(query_words) + len(document_words) - intersection
-    
-#     similarity = intersection / union
-#     return similarity
-
-# # Contoh penggunaan
-# query = "jembatan yang lurus"
-# document = "Tunjukilah kami jalan yang lurus, (yaitu) jalan orang-orang yang telah Engkau beri nikmat kepadanya; bukan (jalan) mereka yang dimurkai, dan bukan (pula jalan) mereka yang sesat."
-
-# similarity_score = semantic_similarity(query, document)
-# print("Kemiripan semantik:", similarity_score)
+# Display the similarity scores and sorted documents
+for document, similarity in results:
+    print(f"Similarity Score: {similarity}")
+    print("Document:", document)
+    print()
