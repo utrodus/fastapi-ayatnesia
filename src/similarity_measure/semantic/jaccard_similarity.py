27961ba1e8c5c38d8028358cc0456a9fd5f3d67d
utrodus <contact.utrodus@gmail.com>
2023-06-21 10:14:16 +0700

    fix: :sparkles: semantic measure process

--- src/similarity_measure/semantic/jaccard_similarity.py
+++
@@ -1,8 +0,0 @@
-class JaccardSimilarity:
-    def calculate(self, query, document):
-        query_set = set(query)
-        document_set = set(document)
-        intersection = query_set.intersection(document_set)
-        union = query_set.union(document_set)
-        similarity = len(intersection) / len(union)
-        return similarity    
--- src/similarity_measure/semantic/semantic_measure.py
+++ src/similarity_measure/semantic/semantic_measure.py
@@ -2,73 +2,47 @@ import sys
 sys.path.append("src")
 from database.database import get_all_ayahs, get_surah_name_by_id
 from similarity_measure.semantic.word_embedding import WordEmbedding
-from similarity_measure.semantic.jaccard_similarity import JaccardSimilarity
 
 class SemanticMeasure:
-    def __init__(self):
-        self.documents = get_all_ayahs()
-        self.results = []
-        self.similarities = {}
-        self.nearest_ayahs_words = []
-        self.word_embedding = WordEmbedding()
-        self.get_doc_nearest_words()
-        # self.w_cosine = 0.7 # Bobot untuk cosine similarity
-        # self.w_jaccard = 0.3 # Bobot untuk Jaccard similarity
+    def __init__(self):              
+        self.word_embedding = WordEmbedding()        
+        self.all_ayahs = get_all_ayahs()
 
     def sort_documents(self):
-        self.similarities = sorted(self.similarities.items(), key=lambda x: x[1], reverse=True)
-
-    def get_query_nearest_words(self, query):
-        self.nearest_query_words = self.word_embedding.get_nearest_words(query)
-
-    def get_doc_nearest_words(self):
-        for doc in self.documents:
-            self.nearest_ayahs_words.append(doc['word_embedding_result'])
+        self.results = sorted(self.results, key=lambda x: x[1], reverse=True)
 
     def calculate_semantic_similarity(self, query):
-        self.get_query_nearest_words(query)
-        query_and_doc_similarity = [self.word_embedding.calculate_similarity(self.nearest_query_words, doc_words) for doc_words in self.nearest_ayahs_words]
-        jaccard_similarity = [JaccardSimilarity().calculate(self.nearest_query_words, doc_words) for doc_words in self.nearest_ayahs_words]
-        for i in range(len(self.documents)):            
-            self.similarities[i] = (query_and_doc_similarity[i] + jaccard_similarity[i]) / 2
-            # (self.w_cosine * query_and_doc_similarity[i] + self.w_jaccard * jaccard_similarity[i]) 
-            # / (self.w_cosine + self.w_jaccard)
-                    
-        return self.similarities
-    
-    def get_top_similarities(self, top_relevance):
-        self.sort_documents()
-        self.results = []
-        def transform_similarity(document_index, similarity):
-            similarity_percentage = similarity * 100
-            return {
-                "surah_id": self.documents[document_index]["surah_id"],
-                "surah_name": get_surah_name_by_id(self.documents[document_index]["surah_id"]),
-                "ayah_arabic": self.documents[document_index]["arabic"],
-                "ayah_translation": self.documents[document_index]["translation"],
-                "number_in_surah": self.documents[document_index]['number']['inSurah'],
-                "tafsir": self.documents[document_index]["tafsir"],
-                "similarity_score": f"{similarity:.4f}",
-                "similarity_percentage": f"{similarity_percentage:.2f}%",
+        results = []
+        query_vector = self.word_embedding.get_words_vector(query)
+        for ayah in self.all_ayahs:
+            ayah_vector = self.word_embedding.get_words_vector(ayah['preprocessed'])
+            similarity = self.word_embedding.calculate_similarity(query_vector, ayah_vector)
+            result = {
+                'id': ayah['id'],
+                'surat_name': get_surah_name_by_id(ayah['surah_id']),
+                'similarity': float(similarity),
+                'arabic': ayah['arabic'],
+                'translation': ayah['translation'],
+                'numberInQuran': ayah['number']['inQuran'],
+                'numberInSurah': ayah['number']['inSurah'],
+                'tafsir': ayah['tafsir']
             }
-
+            results.append(result)
+        results = sorted(results, key=lambda x: x['similarity'], reverse=True)            
+        return results
+    
+    def get_top_similarities(self, query, top_relevance):
+        results = self.calculate_semantic_similarity(query)
         if top_relevance == "all":
-            self.results = list(
-                map(transform_similarity, *zip(*self.similarities))
-            )
-        else:
-            top_relevance = int(top_relevance)
-            self.results = list(
-                map(transform_similarity, *zip(*self.similarities[:top_relevance]))
-            )
-                        
-        return self.results
+            top_results = results
+        else: 
+            top_relevance = int(top_relevance)                   
+            top_results = results[:top_relevance]
+        return top_results                    
 
-# query = "Dengan Menyebut Nama Allah Yang Maha Pengasih Lagi Maha Penyayang"
-# query_preprocessed = Preprocessing(query).execute()
+# example usage    
 # semantic_measure = SemanticMeasure()
-# semantic_measure.calculate_semantic_similarity(query_preprocessed)
-# results = semantic_measure.get_top_similarities()
-# for result in results:
-#     print(result)
-            
+# query = ['istimewa', 'hewan', 'ternak', 'alquran']
+# top_results = semantic_measure.get_top_similarities(query)    
+# for i in top_results:
+#     print(i['surat_name'], i['arabic'], i['translation'], i['tafsir'],i['similarity'])
